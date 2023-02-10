f49cba0e9adfd4d181bb7993c3e6febb92d9c781
bohdan <bohdan@Bohdans-MacBook-Pro.local>
2023-02-10 08:45:22 +0200

    Small note

--- app/models/user_custom_field.rb
+++ app/models/user_custom_field.rb
@@ -3,6 +3,8 @@
 # Representing dynamic field that can be set for {User}.
 #
 class UserCustomField < ApplicationRecord
+  # TODO: Implement DSL for validations, ex:
+  #   validate_value_with UserCustomFieldMultipleSelectValidator, options
   def self.validator; end
 
   def validator_options
